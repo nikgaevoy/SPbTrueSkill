9cffa873962862886bbbc4bb4a70e247dc692f77
Nikita Gaevoy <nikgaevoy@gmail.com>
2020-06-28 03:19:35 +0300

    Fix overloading boilerplate

--- src/rating_system/distributions.rs
+++ src/rating_system/distributions.rs
@@ -1,4 +1,7 @@
-use core::ops;
+extern crate overload;
+
+use overload::overload;
+use std::ops;
 
 use statrs::function::erf::erfc;
 use std::f64::consts::PI;
@@ -11,331 +14,94 @@ pub struct Gaussian {
     pub sigma: f64,
 }
 
-impl ops::Add<Gaussian> for Gaussian {
-    type Output = Gaussian;
-
-    fn add(self, to: Gaussian) -> Gaussian {
-        Gaussian {
-            mu: self.mu + to.mu,
-            sigma: (self.sigma.powi(2) + to.sigma.powi(2)).sqrt(),
-        }
+overload!((a: ?Gaussian) + (b: ?Gaussian) -> Gaussian {
+    Gaussian {
+        mu: a.mu + b.mu,
+        sigma: (a.sigma.powi(2) + b.sigma.powi(2)).sqrt(),
     }
-}
+});
 
+overload!((a: &mut Gaussian) += (b: ?Gaussian) {
+    a.mu += b.mu;
+    a.sigma = (a.sigma.powi(2) + b.sigma.powi(2)).sqrt();
+});
 
-impl<'a> ops::Add<Gaussian> for &'a Gaussian {
-    type Output = Gaussian;
-
-    fn add(self, to: Gaussian) -> Gaussian {
-        Gaussian {
-            mu: self.mu + to.mu,
-            sigma: (self.sigma.powi(2) + to.sigma.powi(2)).sqrt(),
-        }
+overload!((a: ?Gaussian) - (b: ?Gaussian) -> Gaussian {
+    Gaussian {
+        mu: a.mu - b.mu,
+        sigma: (a.sigma.powi(2) + b.sigma.powi(2)).sqrt(),
     }
-}
+});
 
-impl<'b> ops::Add<&'b Gaussian> for Gaussian {
-    type Output = Gaussian;
+overload!((a: &mut Gaussian) -= (b: ?Gaussian) {
+    a.mu -= b.mu;
+    a.sigma = (a.sigma.powi(2) + b.sigma.powi(2)).sqrt();
+});
 
-    fn add(self, to: &'b Gaussian) -> Gaussian {
-        Gaussian {
-            mu: self.mu + to.mu,
-            sigma: (self.sigma.powi(2) + to.sigma.powi(2)).sqrt(),
-        }
+
+overload!((a: ?Gaussian) * (b: ?f64) -> Gaussian {
+    Gaussian {
+        mu: a.mu * b,
+        sigma: a.sigma * b.abs(),
     }
-}
+});
 
+overload!((a: &mut Gaussian) *= (b: ?f64) {
+    a.mu *= b;
+    a.sigma *= b.abs();
+});
 
-impl<'a, 'b> ops::Add<&'b Gaussian> for &'a Gaussian {
-    type Output = Gaussian;
-
-    fn add(self, to: &'b Gaussian) -> Gaussian {
-        Gaussian {
-            mu: self.mu + to.mu,
-            sigma: (self.sigma.powi(2) + to.sigma.powi(2)).sqrt(),
-        }
+overload!((a: ?Gaussian) / (b: ?f64) -> Gaussian {
+    Gaussian {
+        mu: a.mu / b,
+        sigma: a.sigma / b.abs(),
     }
-}
+});
+
+overload!((a: &mut Gaussian) /= (b: ?f64) {
+    a.mu /= b;
+    a.sigma /= b.abs();
+});
 
 
-impl ops::Sub<Gaussian> for Gaussian {
-    type Output = Gaussian;
-
-    fn sub(self, to: Gaussian) -> Gaussian {
-        Gaussian {
-            mu: self.mu - to.mu,
-            sigma: (self.sigma.powi(2) + to.sigma.powi(2)).sqrt(),
-        }
+overload!((a: ?Gaussian) * (b: ?Gaussian) -> Gaussian {
+    let ssigma1 = a.sigma.powi(2);
+    let ssigma2 = b.sigma.powi(2);
+    Gaussian {
+        mu: (a.mu * ssigma2 + b.mu * ssigma1) / (ssigma1 + ssigma2),
+        sigma: a.sigma * b.sigma / (ssigma1 + ssigma2).sqrt(),
     }
-}
+});
 
+overload!((a: &mut Gaussian) *= (b: ?Gaussian) {
+    *a = a.clone() * b;
+});
 
-impl<'a> ops::Sub<Gaussian> for &'a Gaussian {
-    type Output = Gaussian;
-
-    fn sub(self, to: Gaussian) -> Gaussian {
-        Gaussian {
-            mu: self.mu - to.mu,
-            sigma: (self.sigma.powi(2) + to.sigma.powi(2)).sqrt(),
-        }
+overload!((a: ?Gaussian) / (b: ?Gaussian) -> Gaussian {
+    let ssigma1 = a.sigma.powi(2);
+    let ssigma2 = b.sigma.powi(2);
+    Gaussian {
+        mu: (a.mu * ssigma2 - b.mu * ssigma1) / (ssigma2 - ssigma1),
+        sigma: a.sigma * b.sigma / (ssigma2 - ssigma1).abs().sqrt(),
     }
-}
+});
 
+overload!((a: &mut Gaussian) /= (b: ?Gaussian) {
+    *a = a.clone() / b;
+});
 
-impl<'b> ops::Sub<&'b Gaussian> for Gaussian {
-    type Output = Gaussian;
-
-    fn sub(self, to: &'b Gaussian) -> Gaussian {
-        Gaussian {
-            mu: self.mu - to.mu,
-            sigma: (self.sigma.powi(2) + to.sigma.powi(2)).sqrt(),
-        }
-    }
-}
-
-
-impl<'a, 'b> ops::Sub<&'b Gaussian> for &'a Gaussian {
-    type Output = Gaussian;
-
-    fn sub(self, to: &'b Gaussian) -> Gaussian {
-        Gaussian {
-            mu: self.mu - to.mu,
-            sigma: (self.sigma.powi(2) + to.sigma.powi(2)).sqrt(),
-        }
-    }
-}
-
-
-impl ops::AddAssign<Gaussian> for Gaussian {
-    fn add_assign(&mut self, to: Gaussian) {
-        *self = Self {
-            mu: self.mu + to.mu,
-            sigma: (self.sigma.powi(2) + to.sigma.powi(2)).sqrt(),
-        };
-    }
-}
-
-
-impl<'b> ops::AddAssign<&'b Gaussian> for Gaussian {
-    fn add_assign(&mut self, to: &'b Gaussian) {
-        *self = Self {
-            mu: self.mu + to.mu,
-            sigma: (self.sigma.powi(2) + to.sigma.powi(2)).sqrt(),
-        };
-    }
-}
-
-
-impl ops::SubAssign<Gaussian> for Gaussian {
-    fn sub_assign(&mut self, to: Gaussian) {
-        *self = Self {
-            mu: self.mu - to.mu,
-            sigma: (self.sigma.powi(2) + to.sigma.powi(2)).sqrt(),
-        };
-    }
-}
-
-
-impl<'b> ops::SubAssign<&'b Gaussian> for Gaussian {
-    fn sub_assign(&mut self, to: &'b Gaussian) {
-        *self = Self {
-            mu: self.mu - to.mu,
-            sigma: (self.sigma.powi(2) + to.sigma.powi(2)).sqrt(),
-        };
-    }
-}
-
-
-impl ops::Mul<f64> for Gaussian {
-    type Output = Gaussian;
-
-    fn mul(self, to: f64) -> Gaussian {
-        Gaussian {
-            mu: self.mu * to,
-            sigma: self.sigma * to.abs(),
-        }
-    }
-}
-
-impl<'b> ops::Mul<&'b f64> for Gaussian {
-    type Output = Gaussian;
-
-    fn mul(self, to: &'b f64) -> Gaussian {
-        Gaussian {
-            mu: self.mu * *to,
-            sigma: self.sigma * to.abs(),
-        }
-    }
-}
-
-
-impl<'a> ops::Mul<f64> for &'a Gaussian {
-    type Output = Gaussian;
-
-    fn mul(self, to: f64) -> Gaussian {
-        Gaussian {
-            mu: self.mu * to,
-            sigma: self.sigma * to.abs(),
-        }
-    }
-}
-
-impl<'a, 'b> ops::Mul<&'b f64> for &'a Gaussian {
-    type Output = Gaussian;
-
-    fn mul(self, to: &'b f64) -> Gaussian {
-        Gaussian {
-            mu: self.mu * *to,
-            sigma: self.sigma * to.abs(),
-        }
-    }
-}
-
-impl ops::Mul<Gaussian> for Gaussian {
-    type Output = Gaussian;
-
-    fn mul(self, to: Gaussian) -> Gaussian {
-        let ssigma1 = self.sigma.powi(2);
-        let ssigma2 = to.sigma.powi(2);
-        Gaussian {
-            mu: (self.mu * ssigma2 + to.mu * ssigma1) / (ssigma1 + ssigma2),
-            sigma: self.sigma * to.sigma / (ssigma1 + ssigma2).sqrt(),
-        }
-    }
-}
-
-impl<'a> ops::Mul<Gaussian> for &'a Gaussian {
-    type Output = Gaussian;
-
-    fn mul(self, to: Gaussian) -> Gaussian {
-        let ssigma1 = self.sigma.powi(2);
-        let ssigma2 = to.sigma.powi(2);
-        Gaussian {
-            mu: (self.mu * ssigma2 + to.mu * ssigma1) / (ssigma1 + ssigma2),
-            sigma: self.sigma * to.sigma / (ssigma1 + ssigma2).sqrt(),
-        }
-    }
-}
-
-impl<'b> ops::Mul<&'b Gaussian> for Gaussian {
-    type Output = Gaussian;
-
-    fn mul(self, to: &'b Gaussian) -> Gaussian {
-        let ssigma1 = self.sigma.powi(2);
-        let ssigma2 = to.sigma.powi(2);
-        Gaussian {
-            mu: (self.mu * ssigma2 + to.mu * ssigma1) / (ssigma1 + ssigma2),
-            sigma: self.sigma * to.sigma / (ssigma1 + ssigma2).sqrt(),
-        }
-    }
-}
-
-impl<'a, 'b> ops::Mul<&'b Gaussian> for &'a Gaussian {
-    type Output = Gaussian;
-
-    fn mul(self, to: &'b Gaussian) -> Gaussian {
-        let ssigma1 = self.sigma.powi(2);
-        let ssigma2 = to.sigma.powi(2);
-        Gaussian {
-            mu: (self.mu * ssigma2 + to.mu * ssigma1) / (ssigma1 + ssigma2),
-            sigma: self.sigma * to.sigma / (ssigma1 + ssigma2).sqrt(),
-        }
-    }
-}
-
-impl ops::MulAssign<Gaussian> for Gaussian {
-    fn mul_assign(&mut self, to: Gaussian) {
-        let ssigma1 = self.sigma.powi(2);
-        let ssigma2 = to.sigma.powi(2);
-
-        *self = Gaussian {
-            mu: (self.mu * ssigma2 + to.mu * ssigma1) / (ssigma1 + ssigma2),
-            sigma: self.sigma * to.sigma / (ssigma1 + ssigma2).sqrt(),
-        };
-    }
-}
-
-impl<'b> ops::MulAssign<&'b Gaussian> for Gaussian {
-    fn mul_assign(&mut self, to: &'b Gaussian) {
-        let ssigma1 = self.sigma.powi(2);
-        let ssigma2 = to.sigma.powi(2);
-
-        *self = Gaussian {
-            mu: (self.mu * ssigma2 + to.mu * ssigma1) / (ssigma1 + ssigma2),
-            sigma: self.sigma * to.sigma / (ssigma1 + ssigma2).sqrt(),
-        };
-    }
-}
-
-
-impl ops::Div<Gaussian> for Gaussian {
-    type Output = Gaussian;
-
-    fn div(self, to: Gaussian) -> Gaussian {
-        let ssigma1 = self.sigma.powi(2);
-        let ssigma2 = to.sigma.powi(2);
-        Gaussian {
-            mu: (self.mu * ssigma2 - to.mu * ssigma1) / (ssigma2 - ssigma1),
-            sigma: self.sigma * to.sigma / (ssigma2 - ssigma1).abs().sqrt(),
-        }
-    }
-}
-
-
-impl<'a> ops::Div<Gaussian> for &'a Gaussian {
-    type Output = Gaussian;
-
-    fn div(self, to: Gaussian) -> Gaussian {
-        let ssigma1 = self.sigma.powi(2);
-        let ssigma2 = to.sigma.powi(2);
-        Gaussian {
-            mu: (self.mu * ssigma2 - to.mu * ssigma1) / (ssigma2 - ssigma1),
-            sigma: self.sigma * to.sigma / (ssigma2 - ssigma1).abs().sqrt(),
-        }
-    }
-}
-
-impl<'b> ops::Div<&'b Gaussian> for Gaussian {
-    type Output = Gaussian;
-
-    fn div(self, to: &'b Gaussian) -> Gaussian {
-        let ssigma1 = self.sigma.powi(2);
-        let ssigma2 = to.sigma.powi(2);
-        Gaussian {
-            mu: (self.mu * ssigma2 - to.mu * ssigma1) / (ssigma2 - ssigma1),
-            sigma: self.sigma * to.sigma / (ssigma2 - ssigma1).abs().sqrt(),
-        }
-    }
-}
-
-
-impl<'a, 'b> ops::Div<&'b Gaussian> for &'a Gaussian {
-    type Output = Gaussian;
-
-    fn div(self, to: &'b Gaussian) -> Gaussian {
-        let ssigma1 = self.sigma.powi(2);
-        let ssigma2 = to.sigma.powi(2);
-        Gaussian {
-            mu: (self.mu * ssigma2 - to.mu * ssigma1) / (ssigma2 - ssigma1),
-            sigma: self.sigma * to.sigma / (ssigma2 - ssigma1).abs().sqrt(),
-        }
-    }
-}
 
 fn gauss_exponent(mu: f64, sigma: f64, t: f64) -> f64 {
     (-((t - mu) / sigma).powi(2)).exp()
 }
 
-
 fn moment0(mu: f64, sigma: f64, t: f64) -> f64 {
     sigma * PI.sqrt() / 2. * erfc((t - mu) / sigma)
 }
 
-
 fn moment1(mu: f64, sigma: f64, t: f64) -> f64 {
     mu * moment0(0., sigma, t - mu) + sigma.powi(2) / 2. * gauss_exponent(mu, sigma, t)
 }
-
 
 fn moment2(mu: f64, sigma: f64, t: f64) -> f64 {
     mu.powi(2) * moment0(0., sigma, t - mu) + 2. * mu * moment1(0., sigma, t - mu) +
